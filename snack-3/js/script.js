8c3cf3c458d3b7d617beea4f389240aad71dcd71
perezalexreal <perezalexreal@gmail.com>
2021-12-15 17:07:07 +0100

    Terzo snack Risolto

---
+++ snack-3/js/script.js
@@ -0,0 +1,11 @@
+
+let potenzaDi = 2;
+
+let currentNumber;
+
+do {
+    currentNumber = Math.pow(potenzaDi, 3);
+    console.log(currentNumber);
+    potenzaDi += 2;
+
+} while (currentNumber <= 1000 ) 
